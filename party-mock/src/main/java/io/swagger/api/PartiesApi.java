c59c50a4a27113690f0009229842704c18c25f5a
Edward Stevens <edward.stevens@ons.gov.uk>
2017-06-05 15:25:57 +0100

    CTPA-1273 updated to return a PartyDTO for ActionDistributor in ActionSvc

--- party-mock/src/main/java/io/swagger/api/PartiesApi.java
+++ party-mock/src/main/java/io/swagger/api/PartiesApi.java
@@ -1,40 +1,33 @@
 package io.swagger.api;
 
-import io.swagger.model.Error;
-import io.swagger.model.Party;
-
 import io.swagger.annotations.*;
+import io.swagger.model.Party;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
+import uk.gov.ons.ctp.response.party.representation.PartyDTO;
 
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringCodegen", date = "2017-05-26T09:46:55.625Z")
 
 @Api(value = "parties", description = "the parties API")
 public interface PartiesApi {
 
-    @ApiOperation(value = "Get a Party by its UUID", notes = "Returns a single Party", response = Party.class, authorizations = {
+    @ApiOperation(value = "Get a Party by its UUID", notes = "Returns a single Party", response = PartyDTO.class, authorizations = {
         @Authorization(value = "accessCode", scopes = {
             @AuthorizationScope(scope = "read", description = "allows reading resources")
             })
     }, tags={ "backstage", })
     @ApiResponses(value = { 
-        @ApiResponse(code = 200, message = "Party representation", response = Party.class),
-        @ApiResponse(code = 401, message = "Unauthorized", response = Party.class),
-        @ApiResponse(code = 403, message = "Forbidden - in the context of frontstage use, this response would indicate the Respondent does not have an ACTIVE association with this business", response = Party.class),
-        @ApiResponse(code = 404, message = "Business not found", response = Party.class) })
+        @ApiResponse(code = 200, message = "Party representation", response = PartyDTO.class),
+        @ApiResponse(code = 401, message = "Unauthorized", response = PartyDTO.class),
+        @ApiResponse(code = 403, message = "Forbidden - in the context of frontstage use, this response would indicate the Respondent does not have an ACTIVE association with this business", response = PartyDTO.class),
+        @ApiResponse(code = 404, message = "Business not found", response = PartyDTO.class) })
     @RequestMapping(value = "/parties/{id}",
-        produces = { "application/vnd.ons.party+json" }, 
+        produces = { "application/json" },
         method = RequestMethod.GET)
-    ResponseEntity<Party> getPartyById(@ApiParam(value = "ID of the Party to return",required=true ) @PathVariable("id") String id);
+    ResponseEntity<?> getPartyById(@ApiParam(value = "ID of the Party to return",required=true ) @PathVariable("id") String id);
 
 
     @ApiOperation(value = "Get a Party by its unique reference (ruref)", notes = "Returns a single Party", response = Party.class, authorizations = {
--- party-mock/src/main/java/io/swagger/api/PartiesApiController.java
+++ party-mock/src/main/java/io/swagger/api/PartiesApiController.java
@@ -1,21 +1,16 @@
 package io.swagger.api;
 
-import io.swagger.model.Error;
+import io.swagger.annotations.ApiParam;
 import io.swagger.model.Party;
-
-import io.swagger.annotations.*;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
+import uk.gov.ons.ctp.response.party.representation.PartyDTO;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringCodegen", date = "2017-05-26T09:46:55.625Z")
@@ -23,9 +18,28 @@ import java.util.List;
 @Controller
 public class PartiesApiController implements PartiesApi {
 
-    public ResponseEntity<Party> getPartyById(@ApiParam(value = "ID of the Party to return",required=true ) @PathVariable("id") String id) {
+    public ResponseEntity<?> getPartyById(@ApiParam(value = "ID of the Party to return",required=true ) @PathVariable("id") String id) {
         // do some magic!
-        return new ResponseEntity<Party>(HttpStatus.OK);
+
+        PartyDTO partyDTO = new PartyDTO();
+        partyDTO.setSampleId(1);
+        partyDTO.setPosition(1);
+        partyDTO.setSize(1);
+        partyDTO.setSampleUnitType("BI");
+        partyDTO.setSampleUnitRef("REF");
+        partyDTO.setId("7bc5d41b-0549-40b3-ba76-42f6d4cf3992");
+
+        Map<String, String> partyAttributes = new HashMap<>();
+        partyAttributes.put("firstName", "Edward");
+        partyAttributes.put("lastName", "Stevens");
+        partyAttributes.put("telephone", "08008547321");
+        partyAttributes.put("emailAddress", "email@ons.gov.uk");
+
+        partyDTO.setAttributes(partyAttributes);
+
+        System.out.println("PARTY PARTY PARTY" + partyDTO.toString());
+
+        return ResponseEntity.ok(partyDTO);
     }
 
     public ResponseEntity<Party> getPartyByRef(@ApiParam(value = "Reference of the Party to return",required=true ) @PathVariable("ruref") String ruref) {
